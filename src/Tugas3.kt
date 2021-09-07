574e6419f9e687ac16a3d42a94505980fb05e04d
Bima Bagaskhoro <bimabagas644@gmail.com>
2021-09-07 20:45:27 +0700

    Update Commit Tugas 5

--- src/Tugas3.kt
+++ src/Tugas3.kt
@@ -3,8 +3,8 @@ fun main(){
     //variable i = 2
     var i = 2
 
-    // menggunakan perintah while loops untuk melakukan perulangan yang flexible,
-    // yang bertujuan untuk melakukan pengecekan kondisi
+    /* menggunakan perintah while loops untuk melakukan perulangan yang flexible,
+     yang bertujuan untuk melakukan pengecekan kondisi */
     while (true) {
         println("Berhenti di Kambing $i")
         i++
@@ -19,8 +19,8 @@ fun main(){
     // menggunakan perintah for loops untuk melakukan perulangan
     for (i in 1..10){
 
-        // *menggunakan "if" untuk mencari nilai ganjil dikarenakan rumusnya di bagi 2
-        // dan berhenti di angka 10 dikarenakan data yang di perintahkan dari angka 1 sampai 10
+        /* *menggunakan "if" untuk mencari nilai ganjil dikarenakan rumusnya di bagi 2
+         dan berhenti di angka 10 dikarenakan data yang di perintahkan dari angka 1 sampai 10 */
 
         if (i % 2 == 0){
             continue
