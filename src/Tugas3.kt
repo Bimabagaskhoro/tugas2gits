24e63484cdf1123710fb3df72e400588fb8bf9b4
Bima Bagaskhoro <bimabagas644@gmail.com>
2021-09-07 20:00:13 +0700

    update comment Tugas 5

--- src/Tugas3.kt
+++ src/Tugas3.kt
@@ -1,15 +1,27 @@
 fun main(){
 
+    //variable i = 2
     var i = 2
+
+    // menggunakan perintah while loops untuk melakukan perulangan yang flexible,
+    // yang bertujuan untuk melakukan pengecekan kondisi
     while (true) {
         println("Berhenti di Kambing $i")
         i++
 
+        // *menggunakan "if" untuk mencari nilai index
+        // serta break steatment jika data sudah di temukan
         if (i < 5){
             break
         }
     }
+
+    // menggunakan perintah for loops untuk melakukan perulangan
     for (i in 1..10){
+
+        // *menggunakan "if" untuk mencari nilai ganjil dikarenakan rumusnya di bagi 2
+        // dan berhenti di angka 10 dikarenakan data yang di perintahkan dari angka 1 sampai 10
+
         if (i % 2 == 0){
             continue
         }
